613cd8e65458fcfc07167e93d8e01ca54c2d4e1a
selah251 <zselahdin@gmail.com>
2022-09-08 13:15:30 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,4 @@
 #!/bin/bash
-#include <stdio.h>
-
-/**
- * * main - Prints a text
- * *
- * * Return: Always 0 (Success)
- * */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of

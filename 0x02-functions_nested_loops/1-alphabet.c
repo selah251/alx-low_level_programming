21adc76255982a5214e9ec3e32645bc8853d6029
selah251 <zselahdin67@gmail.com>
2022-09-13 22:23:14 +0300

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *  * print_alphabet - function to print abc
- *   *
- *    * Return: 0
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
  */
 void print_alphabet(void)
 {

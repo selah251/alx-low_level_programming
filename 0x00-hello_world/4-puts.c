4a080232a405536555dff117e2610ad8d0101ec6
selah251 <zselahdin67@gmail.com>
2022-09-13 07:09:37 +0300

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+/**
+ * *main-Prints
+ * */
 
 int main(void)
 {
-	puts(" \"Programming is like building a multilingual puzzle\n");
-	
-		return(0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 
 
 }
